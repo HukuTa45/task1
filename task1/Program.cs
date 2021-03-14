4d8d5d9c8a73afa0c3223fc20c9e28485586516c
HukuTa45 <nikitakuzmin20011@mail.ru>
2021-03-15 00:45:39 +0500

    Доработаны функции, исправлена ошибка с корнем

--- task1/Program.cs
+++ task1/Program.cs
@@ -14,7 +14,7 @@ namespace task1
             string age = Console.ReadLine();
 
             //Проверяем значение возраста (число да/нет)
-            while (double.TryParse(age, out double numericAge) == false || double.Parse(age) <=0){
+            while (double.TryParse(age, out double numericAge) == false || double.Parse(age) <=0) {
                 Console.WriteLine(" Введите число, которое больше нуля ");
                 age = Console.ReadLine();
             }
@@ -22,15 +22,11 @@ namespace task1
             string number = Console.ReadLine();
 
             //Проверяем на корректность введенное значение           
-            while (double.TryParse(number, out double numberInt) == false)
-            {
-
+            while (double.TryParse(number, out double numberInt) == false) { 
                 Console.WriteLine(" Некорректные данные! Введите число от 3 до 9!");
                 number = Console.ReadLine();
             }
-            while (double.Parse(number) < 3 || double.Parse(number) > 9)
-            {
-
+            while (double.Parse(number) < 3 || double.Parse(number) > 9) {
                 Console.WriteLine(" Введите число от 3 до 9!");
                 number = Console.ReadLine();
             }
@@ -47,7 +43,7 @@ namespace task1
         }
 
             // функция для проверки чётности
-         static string Parity(double number){            
+         static string Parity(double number) {            
                 if (number % 2 == 0){ 
                     return "чётное число";
                 }
@@ -57,10 +53,9 @@ namespace task1
         }
 
             //функция проверки деления квадрата возраста на число
-        static string divisibilityOfaNumber(double age, double number){
-            double sqrtAge = Math.Ceiling(Math.Sqrt(age));
-            Console.WriteLine("Корень    " + sqrtAge);
-                if (sqrtAge % number == 0){
+        static string divisibilityOfaNumber(double age, double number) {
+            double sqrtAge = Math.Ceiling(Math.Sqrt(age));           
+                if (sqrtAge % number == 0) {
                     return "делится";
                 }
                 else{
